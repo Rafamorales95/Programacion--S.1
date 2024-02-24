eb395dbd0717405994f78d3258ff17ae2d6334d9
Rafamorales95 <161092762+Rafamorales95@users.noreply.github.com>
2024-02-23 19:24:44 -0500

    Add files via upload

---
+++ Rafa.py #1.py
@@ -0,0 +1,14 @@
+numero1 = 10
+numero2 = 20
+
+
+suma = 10 + 20
+resta = 10 - 20
+multiplicacion = 10 * 20
+division = 10 / 20
+
+
+print (suma)
+print (resta)
+print (multiplicacion)
+print (division)
