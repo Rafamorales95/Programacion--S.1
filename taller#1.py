69f443131a1f10861687a20a1b44db1103d9911d
Rafamorales95 <161092762+Rafamorales95@users.noreply.github.com>
2024-03-04 19:51:21 -0500

    Calificacion

---
+++ taller#1.py
@@ -0,0 +1,14 @@
+nota = input('ingrese su nota del 1 al 5')
+nota = float(nota)
+
+if nota >= 5.0 and 4.5:
+    print("Su Nota Es Excelente")
+    print(" Tienes un 20% de decuento")
+
+elif nota >= 3.5 and 3.0:
+    print("su nota es sobre saliente")
+    print("Pagas el 100% de la matricula")
+
+elif nota >= 1.0 and 2.5:
+    print("su nota es deficiente")
+    print("Pagas el 100% de la matricula")
