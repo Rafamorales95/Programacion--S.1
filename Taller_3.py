457594578c4fd599729f585c5a9a12d3d951caab
Rafamorales95 <161092762+Rafamorales95@users.noreply.github.com>
2024-03-12 20:23:26 -0500

    ACTIVIDAD DESEMPEÑO

---
+++ Taller_3.py
@@ -0,0 +1,16 @@
+nota = input('ingrese su nota del 1 al 5')
+nota = float(nota)
+
+if nota >= 5.0 and 4.5:
+    print("Su Nota Es Excelente")
+    print("se otorga un descuento en la matrícula del 20%")
+
+elif nota >= 3.5 and 3.0:
+    print("su nota es sobre saliente")
+    print("pagas el 100% de la matricula")
+
+elif nota >= 1.0 and 2.0:
+    print("su nota es deficiente")
+    print("pagas el 100% de la matricula")
+
+
